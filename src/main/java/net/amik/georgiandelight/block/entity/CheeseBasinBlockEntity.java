177ecfcc179442936777ddcb24d4a60457fa2915
Amiran <amikmain001@gmail.com>
2023-03-10 12:12:29 +0400

    Deleted imports

--- src/main/java/net/amik/georgiandelight/block/entity/CheeseBasinBlockEntity.java
+++ src/main/java/net/amik/georgiandelight/block/entity/CheeseBasinBlockEntity.java
@@ -1,29 +1,5 @@
 package net.amik.georgiandelight.block.entity;
 
-import net.amik.georgiandelight.item.ModItems;
-import net.amik.georgiandelight.screen.CheeseBasinMenu;
-import net.minecraft.core.BlockPos;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TextComponent;
-import net.minecraft.world.Containers;
-import net.minecraft.world.MenuProvider;
-import net.minecraft.world.SimpleContainer;
-import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.items.CapabilityItemHandler;
-import net.minecraftforge.items.IItemHandler;
-import net.minecraftforge.items.ItemStackHandler;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
 public class CheeseBasinBlockEntity {
 
 }
